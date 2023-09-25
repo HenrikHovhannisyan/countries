225d501a60676d6227f50b0b7d8aab3a8ccc3092
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-09-25 16:16:06 +0400

    Added CountriesList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import './App.css';
 import CountriesList from "./components/countriesList";
 
 function App() {
-  return (
-    <CountriesList />
-  );
+    return (
+        <CountriesList/>
+    );
 }
 
 export default App;
--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const API = "https://raw.githubusercontent.com/iamspruce/search-filter-painate-reactjs/main/data/countries.json";
+export const COUNTRY_LIST = "https://raw.githubusercontent.com/iamspruce/search-filter-painate-reactjs/main/data/countries.json";
